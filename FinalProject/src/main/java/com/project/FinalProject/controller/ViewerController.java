d809da150f0d34fa7e0cb6f8a0123c3bf3a163ff
crazzyleucocyte <alstjr7696@naver.com>
2024-10-13 15:22:32 +0900

    .

---
+++ FinalProject/src/main/java/com/project/FinalProject/controller/ViewerController.java
@@ -0,0 +1,17 @@
+package com.project.FinalProject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ViewerController {
+	
+	@Autowired
+	ViewerService viewerService;
+	
+	@GetMapping("viewer/{studyKey}/{seriesKey}/{imageKey")
+	private String test(@PathVariable(name="studyKey") String studyKey ,@PathVariable)
+
+}
