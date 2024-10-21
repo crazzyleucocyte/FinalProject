95f9eb5f7477a05c7448fb9b65fdc4fa47b63921
crazzyleucocyte <alstjr7696@naver.com>
2024-10-21 18:11:07 +0900

    Update StudyTabController.java

--- FinalProject/src/main/java/com/project/FinalProject/controller/StudyTabController.java
+++ FinalProject/src/main/java/com/project/FinalProject/controller/StudyTabController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -26,6 +27,11 @@ public class StudyTabController {
 	@RequestMapping("/")
 	private String root() {
 		return "login";
+	}
+	
+	@PostMapping("/login")
+	private String login() {
+		return "home";
 	}
 	
 	@RequestMapping("/list")
