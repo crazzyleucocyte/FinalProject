c6859c7b20d45836e19e6a0a8a6ef383d41dfece
ohayo <qkzkel17@naver.com>
2024-10-22 10:59:29 +0900

    2024.10.22_10:59(중첩검색가능)

--- FinalProject/src/main/java/com/project/FinalProject/controller/StudyTabController.java
+++ FinalProject/src/main/java/com/project/FinalProject/controller/StudyTabController.java
@@ -42,96 +42,22 @@ public class StudyTabController {
 		return "list";
 	}
 	
-	// 검색 기능 추가
-    @GetMapping("/search")
-    public String searchStudy(@RequestParam(value = "pId", required = false) String pId,
-                                   @RequestParam(value = "pName", required = false) String pName,
-                                   @RequestParam(value = "startDate", required = false) String startDate,
-                                   @RequestParam(value = "endDate", required = false) String endDate,
-                                   @RequestParam(value = "reportStatus", required = false) String reportStatus,
-                                   @RequestParam(value = "modality", required = false) String modality,
-                                   @RequestParam(value = "verifyFlag", required = false) String verifyFlag,
-                                   Model model) {
+	// 중첩된 검색 가능
+	@GetMapping("/search")
+    public String searchStudies(@RequestParam(value = "pId", required = false) String pId,
+                                @RequestParam(value = "pName", required = false) String pName,
+                                @RequestParam(value = "modality", required = false) String modality,
+                                @RequestParam(value = "startDate", required = false) String startDate,
+                                @RequestParam(value = "endDate", required = false) String endDate,
+                                @RequestParam(value = "reportStatus", required = false) Long reportStatus,
+                                @RequestParam(value = "verifyFlag", required = false) Long verifyFlag,
+                                Model model) {
+
         
         
-//      // pId가 비어있을 때 에러 메시지 반환
-//      if ((pId == null || pId.isEmpty()) && (pName == null || pName.isEmpty()) && (studyDate == null || studyDate.isEmpty())) {
-//            model.addAttribute("error", "Patient ID or Name is required");
-//            return "searchResult";
-//      }
-        
-        // pId로 검색
-        if (pId != null && !pId.isEmpty()) {
-            List<StudyTab> studies = studyTabService.findByPId(pId);
-            if (!studies.isEmpty()) {
-                model.addAttribute("studyTabs", studies);
-            } else {
-                model.addAttribute("error", "No study found with the given ID");
-                }
-            }
-        
-        // pName으로 검색
-        if (pName != null && !pName.isEmpty()) {
-            List<StudyTab> studies = studyTabService.findByPName(pName);
-            if (!studies.isEmpty()) {
-                model.addAttribute("studyTabs", studies);
-            } else {
-                model.addAttribute("error", "No study found with the given Name");
-                }
-            }
-        
-        
-        // studyDate로 날짜범위로 검색
-        if (startDate != null && !startDate.isEmpty() && endDate != null && !endDate.isEmpty()) {
-            // 'YYYY-MM-DD' 형식을 'YYYYMMDD'로 변환
-            String formattedStartDate = startDate.replace("-", "");
-            String formattedEndDate = endDate.replace("-", "");
-            // 디버깅 메시지
-            System.out.println("Start date: " + formattedStartDate);
-            System.out.println("End date: " + formattedEndDate);
-            // 날짜 범위로 검색
-            List<StudyTab> studiesByDateRange = studyTabService.findByStudyDateBetween(formattedStartDate, formattedEndDate);
-            if (!studiesByDateRange.isEmpty()) {
-                model.addAttribute("studyTabs", studiesByDateRange);
-                } else {
-                    model.addAttribute("error", "No study found in the given date range");
-                    }
-            }
-        
-        // reportStatus로 판독 상태 검색
-        if (reportStatus != null && !reportStatus.isEmpty()) {
-            Long reportStatusLong = Long.parseLong(reportStatus);
-            List<StudyTab> studiesByStatus = studyTabService.findByReportStatus(reportStatusLong);
-            if (!studiesByStatus.isEmpty()) {
-                model.addAttribute("studyTabs", studiesByStatus);
-                } else {
-                    model.addAttribute("error", "No study found with the given report status");
-                    }
-            }
-        
-        // 검사 장비로 검색
-        if (modality != null && !modality.isEmpty()) {
-            List<StudyTab> studiesByModality = studyTabService.findByModality(modality);
-            if (!studiesByModality.isEmpty()) {
-                model.addAttribute("studyTabs", studiesByModality);
-                } else {
-                    model.addAttribute("error", "No study found with the given modality");
-                    }
-            }
-        
-        // verifyFlag로 검색
-        if (verifyFlag != null && !verifyFlag.isEmpty()) {
-            Long verifyFlagLong = Long.parseLong(verifyFlag);  // 문자열을 Long 타입으로 변환
-            List<StudyTab> studiesByVerifyFlag = studyTabService.findByVerifyFlag(verifyFlagLong);
-            if (!studiesByVerifyFlag.isEmpty()) {
-                model.addAttribute("studyTabs", studiesByVerifyFlag);
-            } else {
-                model.addAttribute("error", "No study found with the given verify status");
-            }
-        }
-        
-        
-        return "searchResult"; // 검색 결과 페이지 반환
+		List<StudyTab> studies = studyTabService.searchStudies(pId, pName, modality, startDate, endDate, reportStatus, verifyFlag);
+        model.addAttribute("studyTabs", studies);
+        return "searchResult";
         }
 	
 }
