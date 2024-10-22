3945d31c07b85c3541d5908f099eb7099d0f1eef
crazzyleucocyte <alstjr7696@naver.com>
2024-10-22 18:35:08 +0900

    10.22 18:35

--- FinalProject/src/main/java/com/project/FinalProject/repository/StudyTabRepository.java
+++ FinalProject/src/main/java/com/project/FinalProject/repository/StudyTabRepository.java
@@ -11,12 +11,12 @@ import com.project.FinalProject.domain.StudyTab;
 
 @Repository
 public interface StudyTabRepository extends JpaRepository<StudyTab, Long> {
-	/*
 	//명시적인 JPQL 쿼리
     @Query("SELECT s FROM StudyTab s WHERE s.pId = :pId")
 	//pId로 검색
     List<StudyTab> findByPId(@Param("pId") String pId);
     
+    /*
     // pName으로 명시적인 JPQL 쿼리 사용
     @Query("SELECT s FROM StudyTab s WHERE s.pName = :pName")
     List<StudyTab> findByPName(@Param("pName") String pName);
@@ -35,7 +35,7 @@ public interface StudyTabRepository extends JpaRepository<StudyTab, Long> {
 */
 	@Query("SELECT s FROM StudyTab s WHERE " +
 		       "(:pId IS NULL OR s.pId = :pId) AND " +
-		       "(:pName IS NULL OR s.pName = :pName) AND " +
+	 	       "(:pName IS NULL OR s.pName = :pName) AND " +
 		       "(:modality IS NULL OR s.modality = :modality) AND " +
 		       "(:startDate IS NULL OR s.studyDate >= :startDate) AND " +
 		       "(:endDate IS NULL OR s.studyDate <= :endDate) AND " +
