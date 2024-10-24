b471ab554fea20f0c4655e936816b6a98f3ba915
0biin0 <infinite402@gmail.com>
2024-10-24 18:45:52 +0900

    RepotTab 테이블 수정

--- FinalProject/src/main/java/com/project/FinalProject/domain/ReportTab.java
+++ FinalProject/src/main/java/com/project/FinalProject/domain/ReportTab.java
@@ -3,17 +3,31 @@ package com.project.FinalProject.domain;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity(name = "REPORTTAB")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Builder
 public class ReportTab {
 	@Id
 	@Column(name = "STUDYKEY")
 	private Long studyKey;
 	
-	@Column(name = "COMMENT")
-	private String comment; //코멘트
+	@Column(name = "REPORT_Comment ")
+	private String reportComment; //코멘트
 	
 	@CreatedDate
 	@Column(name = "REPORT_TIME", updatable = false)
