761024e992957cbe09d3c98c165ee7b14cd4db6e
crazzyleucocyte <alstjr7696@naver.com>
2024-11-07 12:49:34 +0900

    11.07 12:49

--- FinalProject/src/main/java/com/project/FinalProject/dto/SearchCondition.java
+++ FinalProject/src/main/java/com/project/FinalProject/dto/SearchCondition.java
@@ -1,11 +1,15 @@
 package com.project.FinalProject.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class SearchCondition {
-
+	
+	@JsonProperty("pId")
 	String pId;
+	@JsonProperty("pName")
 	String pName;
 	String modality;
 	String startDate;
